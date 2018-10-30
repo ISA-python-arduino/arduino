d57855a81463deb034d4842b6e1e48c23ad9d2cf
lukasz <202118@edu.p.lodz.pl>
2018-10-30 15:39:03 +0100

    arduino file changed

--- serial/serial.ino
+++ serial/serial.ino
@@ -5,15 +5,15 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
+  Serial.setTimeout(50);
   pinMode(led, OUTPUT);
   
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    int x = Serial.read() + 96;
-    int mapped_x = map(x, 1, 192, 0, 255);
-    analogWrite(led, mapped_x);
+    int x = Serial.parseInt();
+    Serial.println(x);
+    digitalWrite(led, x < 0 ? HIGH : LOW);
   }
 }
