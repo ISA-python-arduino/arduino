3ebf22a08e0bd8aef5e2c98a4f2e30a599e11dc9
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2018-10-30 15:07:13 +0100

    serial

--- serial/serial.ino
+++ serial/serial.ino
@@ -1,13 +1,19 @@
+const int led = 13;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
+  pinMode(led, OUTPUT);
+  
 }
 
 void loop() {
-  if (Serial.available()) {
-    Serial.write(Serial.read());
+  if (Serial.available() > 0) {
+    int x = Serial.read() + 96;
+    int mapped_x = map(x, 1, 192, 0, 255);
+    analogWrite(led, mapped_x);
   }
 }
