991883df8600fc25e7ff95bcedde8254b4a2adc2
lukasz <202118@edu.p.lodz.pl>
2018-11-27 14:41:44 +0100

    package sending

--- Autko/Autko.ino
+++ Autko/Autko.ino
@@ -1,3 +1,6 @@
+// Example 5 - parsing text and numbers with start and end markers in the stream
+
+#include "helper.h"
 #include <Wire.h> 
 #include "Definitions.h"
 
@@ -8,6 +11,14 @@ const unsigned int frontSensorTrigger = 8;
 const unsigned int frontSensorEcho = 9;
 const unsigned int backSensorTrigger = 10;
 const unsigned int backSensorEcho = 11;
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+dataPacket packet;
+
+boolean newData = false;
+
 
 void setup() {
   Serial.begin(57600);
@@ -144,6 +155,7 @@ struct control {
   unsigned char s;
 };
 struct control c;
+/*
 void loop() {
   if (Serial.available() > 0) {
     String code = Serial.readStringUntil(';');
@@ -156,4 +168,82 @@ void loop() {
     Serial.print(' ');
     Serial.println(c.code);
   }
+}
+*/
+
+
+/*z repo ISA */
+void loop() {
+    recvWithStartEndMarkers();
+    if (newData == true) {
+        strcpy(tempChars, receivedChars);
+            // this temporary copy is necessary to protect the original data
+            //   because strtok() used in parseData() replaces the commas with \0
+        packet = parseData();
+        showParsedData(packet);
+        newData = false;
+    }
+}
+
+//============
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+//============
+
+dataPacket parseData() {      // split the data into its parts
+
+    dataPacket tmpPacket;
+
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    strcpy(tmpPacket.message, strtokIndx); // copy it to messageFromPC
+ 
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    tmpPacket.cordX = atoi(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ",");
+    tmpPacket.cordY = atoi(strtokIndx);     // convert this part to a float
+    return tmpPacket;
+}
+
+
+void showParsedData(dataPacket packet) {
+    Serial.print("Message ");
+    Serial.println(packet.message);
+    Serial.print("CORDX ");
+    Serial.println(packet.cordX);
+    Serial.print("CORDY ");
+    Serial.println(packet.cordY);
 }
